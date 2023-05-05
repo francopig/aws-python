9b6c6873e8e6bc03c4aebb967a16cb9e8761e6c4
Franco Pignanelli <franqo1808@gmail.com>
2023-05-05 20:47:53 -0300

    03. cadenas: lab 110 solved and documented

---
+++ 03. Cadenas/Lab110.py
@@ -0,0 +1,17 @@
+# myString = "Saludos amigos del interweb"
+# print(myString)
+# print(type(myString))
+# print(myString + " is of the data type " + str(type(myString)))
+
+# firstString = "Asimov"
+# secondString = "Code"
+# thirdString = firstString + secondString
+# print(thirdString)
+
+# name = input("Ingresa tu nombre: ")
+# print("Hola " + name)
+
+name = input("Ingresa tu nombre: ")
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
