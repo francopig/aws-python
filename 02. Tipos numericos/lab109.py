ce062c89d759ab9a710ef771d01034ccc2bfd6ab
Franco Pignanelli <franqo1808@gmail.com>
2023-05-05 21:15:02 -0300

    Lab 109: added

---
+++ 02. Tipos numericos/lab109.py
@@ -0,0 +1,15 @@
+# print("Python has three numeric types: int, float, and complex")
+
+
+# myValue = 5
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# myValue = 3.1415
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# myValue = 5j
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=True
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
