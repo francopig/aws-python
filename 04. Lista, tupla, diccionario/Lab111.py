973219abb324a071f62a6404aa7a9f48c55760cb
Franco Pignanelli <franqo1808@gmail.com>
2023-05-08 19:15:50 -0300

    Lab 11: finished

---
+++ 04. Lista, tupla, diccionario/Lab111.py
@@ -0,0 +1,37 @@
+
+# --- Tipo de dato lista ---
+# myFruitList = ["Apple", "Banana", "Cucumelo"];
+# print(myFruitList)
+# print(type(myFruitList))
+
+# print(myFruitList[0])
+# print(myFruitList[1])
+# print(myFruitList[2])
+
+# myFruitList[2] = "Roberto Carlo";
+# print(myFruitList)
+
+
+# --- Tipo de dato Tupla ---
+
+# myFinalAnswerTuple = ("apple", "banano", "pineapple")
+# print(myFinalAnswerTuple)
+# print(type(myFinalAnswerTuple))
+# print("Posición 1:", myFinalAnswerTuple[0])
+# print("Posición 2:", myFinalAnswerTuple[1])
+# print("Posición 3:", myFinalAnswerTuple[2])
+
+# --- Tipo de dato diccionario ---
+
+myFavoriteFruitDictionary = {
+  "Pedro Pascal" : "apple",
+  "Rick" : "banana",
+  "Paul" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Pedro Pascal"])
+print(myFavoriteFruitDictionary["Rick"])
+print(myFavoriteFruitDictionary["Paul"])
